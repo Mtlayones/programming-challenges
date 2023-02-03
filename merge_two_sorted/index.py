2342bb0128341842dd33f850db1ca30f4b1a966d
mtlayones <mtlayones@up.edu.ph>
2023-02-03 14:33:49 +0800

    refactor merge two sorted list

--- merge_two_sorted/index.py
+++ merge_two_sorted/index.py
@@ -13,7 +13,7 @@ class Solution:
         head = ListNode(val=0, next=None)
         temp = head
         while list1 != None or list2 != None:
-            if (list2 != None and list1 != None and list1.val < list2.val) or (list2 == None and list != None):
+            if (list2 != None and list1 != None and list1.val < list2.val) or (list2 == None and list1 != None):
                 temp.next = list1
                 list1 = list1.next
             else:

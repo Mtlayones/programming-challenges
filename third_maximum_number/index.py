4d7b80cd0565c251b06d0ee3823bbb3f63607991
mtlayones <mtlayones@up.edu.ph>
2023-05-10 21:36:02 +0800

    solve third maximum number

---
+++ third_maximum_number/index.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = sorted(list(set(nums)))
+        if len(nums) < 3:
+            return nums[-1]
+        else:
+            return nums[-3]
